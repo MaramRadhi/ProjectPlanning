fbb128c5d7cb0d16df7761229be042ab1011bc64
imrun10 <126344526+MaramRadhi@users.noreply.github.com>
2023-05-08 23:37:53 +0300

    final update v final

---
+++ GradeCourseApp/client/src/components/Courses.js
@@ -0,0 +1,59 @@
+import 'bootstrap/dist/css/bootstrap.min.css'; // importing bootstrap
+import { useNavigate } from "react-router-dom"; // importing react-router-dom so we can link to other pages with data
+ 
+
+import './style/componentsCSS.css' // importing css file
+
+function Courses(props) { /*props is the data that we send to the this component*/
+    const navigate = useNavigate(); // intiales an instance of the useNavigate function and use this instance to route
+
+
+  
+
+    
+    const toCourse = () => {
+        console.log(props);
+     
+      navigate("course", {state: props}); // navigate to the course page and send the course data (props [object]) to the course page
+    };
+
+    const toCoursPage = () => {
+
+        if (props.id === "Student"){
+            navigate("/Students");}
+        else if (props.id === "Course"){
+            navigate("/coursePage");}
+        else if (props.id === "Admin"){
+            navigate("/Admin");
+        }
+        
+
+
+    }
+
+    const pickPage = () => {
+        if(props.code == 1){
+            toCoursPage()
+        }else{
+            toCourse()
+        }
+    }
+
+    return (
+        <a onClick={()=>{pickPage()}}>    {/*when the user click on the course card it will call the toCourse function*/}        
+            <div class="card course-card border-transparent">
+                <div class="card-body">
+                    <h5 class="card-title">{props.name}</h5>    {/*display the course name*/}
+                    <p class="card-text">{props.id}</p>     {/*display the course id*/}
+                    <p class="card-text">{props.proff}</p>  {/*display the course proff*/}
+                </div>
+            </div>
+        </a> 
+
+  
+
+        )}
+
+
+export default Courses;
+// Path: client/src/components/courses.js
---
+++ GradeCourseApp/client/src/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import CoursePage from "./pages/Courses/CoursePage";
+import StudentSummaryPage from "./pages/students/StudentSummaryPage";
+import NotFound from "./pages/Admin/PageNotFound";
+import Register from "./pages/Admin/SignUp";
+import Switch from "react-router-dom";
+import { CookiesProvider } from 'react-cookie';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import Adm from "./pages/Admin/Admin";
+import MainPage from "./pages/MainPage";
+import StudentDetail from "./pages/students/StudentDetail";
+import CourseDetails from "./pages/Courses/courseDetails";
+import CourseSummary from "./pages/Courses/CourseSummary";
+import { AuthProvider } from "react-auth-kit";
+import { RequireAuth } from "react-auth-kit";
+import { Routes } from "react-router-dom";
+import  Login  from "./pages/Admin/signIn";
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <CookiesProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/coursePage" element={<App />} />
+        <Route path="/coursePage/course" element={<CoursePage />} />
+        <Route path="/coursePage/course/CourseDetails" element={<CourseDetails />} />
+        <Route path="/coursePage/course/student" element={<StudentSummaryPage />} />
+        <Route path="/admin" element={<Adm />} />
+        <Route path="/Students" element={<StudentDetail />} />
+        <Route path="/Students/student" element={<StudentSummaryPage />} />
+        <Route path="/coursePage/course/summary" element={<CourseSummary />} />
+
+        <Route path="/register" element={<Register />} />
+      
+       {/* <Route path="/CoursePage/course/student" element={<StudentSummaryPage />} />*/}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+    </CookiesProvider>
+ 
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
